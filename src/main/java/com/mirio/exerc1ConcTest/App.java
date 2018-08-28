7137d868b520cd9863fcf66df8843a785b78ba25
MARIO AUGUSTO BRUM DA SILVEIRA <17104166@pucrs.br>
2018-08-28 10:12:25 -0300

    primeiro commit

---
+++ src/main/java/com/mirio/exerc1ConcTest/App.java
@@ -0,0 +1,13 @@
+package com.mirio.exerc1ConcTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
